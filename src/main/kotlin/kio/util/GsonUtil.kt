fbe209ca1c8a57ae2e5f736cf0a7dfb03715461e
Pawel Pastuszak <kotcrab@gmail.com>
2018-10-22 00:51:46 +0200

    Add RuntimeTypeAdapterFactory

--- src/main/kotlin/kio/util/GsonUtil.kt
+++ src/main/kotlin/kio/util/GsonUtil.kt
@@ -17,17 +17,22 @@
 package kio.util
 
 import com.google.common.io.BaseEncoding
-import com.google.common.reflect.TypeToken
 import com.google.gson.*
+import com.google.gson.internal.Streams
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
 import java.io.File
 import java.io.Reader
 import java.lang.reflect.Type
+import kotlin.collections.set
+import kotlin.reflect.KClass
 
 /** @author Kotcrab */
 
 val stdGson = createStdGson()
 
-fun createStdGson(prettyPrint: Boolean = true, byteArrayAsBase64: Boolean = true): Gson {
+fun createStdGson(prettyPrint: Boolean = true, byteArrayAsBase64: Boolean = true, configure: GsonBuilder.() -> Unit = {}): Gson {
     val builder = GsonBuilder()
     if (prettyPrint) {
         builder.setPrettyPrinting()
@@ -35,6 +40,7 @@ fun createStdGson(prettyPrint: Boolean = true, byteArrayAsBase64: Boolean = true
     if (byteArrayAsBase64) {
         builder.registerTypeAdapter(ByteArray::class.java, ByteArrayAsBase64TypeAdapter())
     }
+    builder.configure()
     return builder.create()
 }
 
@@ -63,4 +69,113 @@ fun File.writeJson(gson: Gson, src: Any) {
 
 inline fun <reified T> File.readJson(gson: Gson = stdGson): T {
     return bufferedReader().use { gson.fromJson(it) }
+}
+
+fun <C : Any> runtimeTypeAdapter(base: KClass<C>,
+                                 subTypes: Array<KClass<out C>>,
+                                 legacySubTypes: Array<Pair<String, KClass<out C>>> = emptyArray()): RuntimeTypeAdapterFactory<C> {
+    val adapter = RuntimeTypeAdapterFactory(base, "_type")
+    subTypes.forEach { subClass ->
+        adapter.registerSubtype(subClass)
+    }
+    legacySubTypes.forEach { subClass ->
+        adapter.registerLegacySubtype(subClass.second, subClass.first)
+    }
+    return adapter
+}
+
+class RuntimeTypeAdapterFactory<T : Any>(
+        private val baseType: KClass<T>,
+        private val typeFieldName: String = "_type"
+) : TypeAdapterFactory {
+    private val labelToSubtype = mutableMapOf<String, KClass<out T>>()
+    private val legacyLabelToSubtype = mutableMapOf<String, KClass<out T>>()
+    private val subtypeToLabel = mutableMapOf<KClass<out T>, String>()
+    private var cachedTypeAdapter: TypeAdapter<*>? = null
+
+    fun <R : T> registerSubtype(type: KClass<R>) {
+        val label = type.simpleName ?: error("type does not provide simple name")
+        registerSubtype(type, label)
+    }
+
+    fun <R : T> registerSubtype(type: KClass<R>, label: String) {
+        if (cachedTypeAdapter != null) {
+            error("can't add subtype, this factory has already created and cached its type adapter")
+        }
+        if (subtypeToLabel.contains(type) || labelToSubtype.contains(label)) {
+            error("types and labels must be unique")
+        }
+        subtypeToLabel[type] = label
+        labelToSubtype[label] = type
+    }
+
+    fun <R : T> registerLegacySubtype(type: KClass<R>, label: String) {
+        if (cachedTypeAdapter != null) {
+            error("can't add legacy subtype, this factory has already created and cached its type adapter")
+        }
+        if (legacyLabelToSubtype.contains(label)) {
+            error("legacy labels must be unique")
+        }
+        legacyLabelToSubtype[label] = type
+    }
+
+    override fun <R> create(gson: Gson, type: TypeToken<R>): TypeAdapter<R>? {
+        if (baseType.java.isAssignableFrom(type.rawType) == false) return null
+        if (cachedTypeAdapter == null) {
+            val labelToDelegate = mutableMapOf<String, TypeAdapter<out T>>()
+            val legacyLabelToDelegate = mutableMapOf<String, TypeAdapter<out T>>()
+            val subtypeToDelegate = mutableMapOf<KClass<out T>, TypeAdapter<out T>>()
+            labelToSubtype.forEach { label, subtype ->
+                val delegate = gson.getDelegateAdapter(this, TypeToken.get(subtype.java))
+                labelToDelegate[label] = delegate
+                subtypeToDelegate[subtype] = delegate
+            }
+            legacyLabelToSubtype.forEach { label, subtype ->
+                val delegate = gson.getDelegateAdapter(this, TypeToken.get(subtype.java))
+                legacyLabelToDelegate[label] = delegate
+            }
+            cachedTypeAdapter = object : TypeAdapter<R>() {
+                override fun write(output: JsonWriter, value: R) {
+                    val srcType = (value as Any)::class
+                    val label = subtypeToLabel[srcType]
+                    @Suppress("UNCHECKED_CAST") // registration requires that subtype extends T
+                    val delegate = subtypeToDelegate[srcType] as TypeAdapter<R>?
+                            ?: throw JsonParseException("cannot serialize ${srcType.simpleName}, did you forget to " +
+                                    "register a subtype?")
+                    val jsonObject = delegate.toJsonTree(value).asJsonObject
+                    val clone = JsonObject()
+                    if (jsonObject.has(typeFieldName)) {
+                        throw JsonParseException("cannot serialize ${srcType.simpleName} because it already defined " +
+                                "a field names $typeFieldName")
+                    }
+                    clone.add(typeFieldName, JsonPrimitive(label))
+                    jsonObject.entrySet().forEach { (key, value) ->
+                        clone.add(key, value)
+                    }
+                    Streams.write(clone, output)
+                }
+
+                override fun read(input: JsonReader): R {
+                    val jsonElement = Streams.parse(input)
+                    val labelJsonElement = jsonElement.asJsonObject.remove(typeFieldName)
+                            ?: throw JsonParseException("cannot deserialize $baseType because it does not define " +
+                                    "a field named $typeFieldName")
+                    val label = labelJsonElement.asString
+                    @Suppress("UNCHECKED_CAST") // registration requires that subtype extends T
+                    var delegate = labelToDelegate[label] as TypeAdapter<R>?
+                    if (delegate == null) {
+                        @Suppress("UNCHECKED_CAST") // registration requires that subtype extends T
+                        delegate = legacyLabelToDelegate[label] as TypeAdapter<R>?
+                        if (delegate == null) {
+                            throw JsonParseException("cannot deserialize $baseType subtype named $label, " +
+                                    "did you forget to register a subtype?")
+                        }
+                    }
+                    return delegate.fromJsonTree(jsonElement)
+                }
+            }
+        }
+        @Suppress("UNCHECKED_CAST")
+        return cachedTypeAdapter as TypeAdapter<R>
+    }
 }
--- src/test/kotlin/kio/util/GsonUtilsTest.kt
+++ src/test/kotlin/kio/util/GsonUtilsTest.kt
@@ -16,13 +16,22 @@
 
 package kio.util
 
-import org.assertj.core.api.Assertions.assertThat
+import com.google.gson.JsonParseException
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.io.StringReader
 
 /** @author Kotcrab */
 
 internal class GsonUtilsTest {
+    @Test
+    fun `should call configure block`() {
+        var called = false
+        createStdGson { called = true }
+        assertThat(called).isTrue()
+    }
+
     @Test
     fun `should read json primitive`() {
         val arr: Int = stdGson.fromJson("1")
@@ -72,4 +81,159 @@ internal class GsonUtilsTest {
         assertThat(str).isNotBlank()
         assertThat(gson.fromJson(str) as ByteArray).containsExactly(0, 1, 2, 3)
     }
+
+    @Nested
+    inner class RuntimeTypeAdapterFactoryTest {
+        @Test
+        fun `should have unique subtypes`() {
+            assertThatIllegalStateException().isThrownBy {
+                RuntimeTypeAdapterFactory(Foo::class).apply {
+                    registerSubtype(Bar::class, "Bar")
+                    registerSubtype(Bar::class, "Bar2")
+                }
+            }
+        }
+
+        @Test
+        fun `should have unique labels`() {
+            assertThatIllegalStateException().isThrownBy {
+                RuntimeTypeAdapterFactory(Foo::class).apply {
+                    registerSubtype(Bar::class, "Label")
+                    registerSubtype(FooBar::class, "Label")
+                }
+            }
+        }
+
+        @Test
+        fun `should have unique legacy labels`() {
+            assertThatIllegalStateException().isThrownBy {
+                RuntimeTypeAdapterFactory(Foo::class).apply {
+                    registerLegacySubtype(Bar::class, "Label")
+                    registerLegacySubtype(FooBar::class, "Label")
+                }
+            }
+        }
+
+        @Test
+        fun `should not serialize class with already existing type field`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(RuntimeTypeAdapterFactory(Foo::class, "type").apply {
+                    registerSubtype(FailFoo::class)
+                })
+            }
+            assertThatExceptionOfType(JsonParseException::class.java).isThrownBy {
+                gson.toJson(arrayOf(FailFoo()))
+            }
+        }
+
+        @Test
+        fun `should not serialize unregistered type`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(RuntimeTypeAdapterFactory(Foo::class, "type").apply {
+                    registerSubtype(Bar::class)
+                })
+            }
+            assertThatExceptionOfType(JsonParseException::class.java).isThrownBy {
+                gson.toJson(Baz())
+            }
+        }
+
+        @Test
+        fun `should not deserialize json with missing type field`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(runtimeTypeAdapter(Foo::class, subTypes = arrayOf(Bar::class, FooBar::class, Baz::class)))
+            }
+            assertThatExceptionOfType(JsonParseException::class.java).isThrownBy {
+                gson.fromJson<Array<Foo>>("[{\"_type\":\"Bar\",\"b\":0,\"a\":0},{\"b\":0,\"a\":0}]")
+            }
+        }
+
+        @Test
+        fun `should not deserialize not registered type`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(RuntimeTypeAdapterFactory(Foo::class, "type").apply {
+                    registerSubtype(Bar::class)
+                    registerSubtype(Baz::class)
+                })
+            }
+            val secondGson = createStdGson {
+                registerTypeAdapterFactory(RuntimeTypeAdapterFactory(Foo::class, "type").apply {
+                    registerSubtype(Bar::class)
+                })
+            }
+            val json = gson.toJson(arrayOf(Bar(), Baz()))
+            assertThatExceptionOfType(JsonParseException::class.java).isThrownBy {
+                secondGson.fromJson<Array<Foo>>(json)
+            }
+        }
+
+        @Test
+        fun `should serialize class hierarchy in list`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(runtimeTypeAdapter(Foo::class, subTypes = arrayOf(Bar::class, FooBar::class, Baz::class)))
+            }
+            val objList = listOf(Bar(), FooBar(), Baz())
+            val json = gson.toJson(objList)
+            val jsonObjList = gson.fromJson<List<Foo>>(json)
+            assertThat(jsonObjList[0]).isInstanceOf(Bar::class.java)
+            assertThat(jsonObjList[1]).isInstanceOf(FooBar::class.java)
+            assertThat(jsonObjList[2]).isInstanceOf(Baz::class.java)
+        }
+
+        @Test
+        fun `should serialize class hierarchy in array`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(runtimeTypeAdapter(Foo::class, subTypes = arrayOf(Bar::class, FooBar::class, Baz::class)))
+            }
+            val objList = arrayOf(Bar(), FooBar(), Baz())
+            val json = gson.toJson(objList)
+            val jsonObjList = gson.fromJson<List<Foo>>(json)
+            assertThat(jsonObjList[0]).isInstanceOf(Bar::class.java)
+            assertThat(jsonObjList[1]).isInstanceOf(FooBar::class.java)
+            assertThat(jsonObjList[2]).isInstanceOf(Baz::class.java)
+        }
+
+        @Test
+        fun `should deserialize legacy label`() {
+            val gson = createStdGson {
+                registerTypeAdapterFactory(RuntimeTypeAdapterFactory(Foo::class).apply {
+                    registerSubtype(Bar::class)
+                })
+            }
+            val newGson = createStdGson {
+                registerTypeAdapterFactory(RuntimeTypeAdapterFactory(Foo::class).apply {
+                    registerSubtype(NewBar::class)
+                    registerLegacySubtype(NewBar::class, "Bar")
+                })
+            }
+            val arr: Array<Foo> = arrayOf(Bar(42))
+            val json = gson.toJson(arr)
+            val desArr = newGson.fromJson<Array<Foo>>(json)
+            assertThat((desArr[0] as NewBar).b).isEqualTo(42)
+        }
+
+        @Test
+        fun `should prevent adding to cached adapter`() {
+            val factory = RuntimeTypeAdapterFactory(Foo::class).apply {
+                registerSubtype(Bar::class)
+            }
+            val gson = createStdGson {
+                registerTypeAdapterFactory(factory)
+            }
+            gson.toJson(arrayOf(Bar()))
+            assertThatIllegalStateException().isThrownBy {
+                factory.registerSubtype(Baz::class)
+            }
+            assertThatIllegalStateException().isThrownBy {
+                factory.registerLegacySubtype(Baz::class, "Baz")
+            }
+        }
+    }
+
+    private open class Foo(val a: Int = 0)
+    private class FailFoo(val type: Int = 0) : Foo()
+    private open class Bar(val b: Int = 0) : Foo()
+    private class NewBar(val b: Int = 0) : Foo()
+    private class FooBar(val c: Int = 0) : Bar()
+    private class Baz(val b: Int = 0) : Foo()
 }
